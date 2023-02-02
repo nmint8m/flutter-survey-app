49950412892e2a2fe3198f75c4690f2475e8c2b7
Tam Nguyen Minh <nmint8m@gmail.com>
2023-02-02 13:51:56 +0700

    [#11] Add base use cases utils

--- lib/usecases/base/use_case_result.dart
+++ lib/usecases/base/use_case_result.dart
@@ -20,4 +20,9 @@ class Failed<T> extends Result<T> {
   final UseCaseException exception;
 
   Failed(this.exception) : super._();
+
+  String get errorMessage => _errorMessage ?? 'Unexpected error happens.';
+
+  String? get _errorMessage =>
+      NetworkExceptions.getErrorMessage(exception.actualException);
 }
