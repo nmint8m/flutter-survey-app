1691c5c95bf5f1c158a9dc3b53e5475517e542fb
Tam Nguyen Minh <nmint8m@gmail.com>
2023-02-03 14:08:56 +0700

    [#38] Add OAuthRepository

---
+++ lib/api/repository/oauth_repository.dart
@@ -0,0 +1,44 @@
+import 'package:injectable/injectable.dart';
+import 'package:kayla_flutter_ic/api/exception/network_exceptions.dart';
+import 'package:kayla_flutter_ic/api/oauth_service.dart';
+import 'package:kayla_flutter_ic/api/request/oauth_login_request.dart';
+import 'package:kayla_flutter_ic/env.dart';
+import 'package:kayla_flutter_ic/model/oauth_login.dart';
+
+abstract class OAuthRepository {
+  Future<OAuthLogin> login({
+    required String email,
+    required String password,
+  });
+}
+
+@Singleton(as: OAuthRepository)
+class OAuthRepositoryImpl extends OAuthRepository {
+  final OAuthService _oauthService;
+
+  OAuthRepositoryImpl(this._oauthService);
+
+  @override
+  Future<OAuthLogin> login(
+      {required String email, required String password}) async {
+    try {
+      final response = await _oauthService.login(
+        OAuthLoginRequest(
+          email: email,
+          password: password,
+          clientId: Env.clientId,
+          clientSecret: Env.clientSecret,
+          grantType: OAuthLoginRequest.passwordGrantType,
+        ),
+      );
+      return OAuthLogin(
+        id: response.id,
+        accessToken: response.accessToken,
+        expiresIn: response.expiresIn,
+        refreshToken: response.refreshToken,
+      );
+    } catch (exception) {
+      throw NetworkExceptions.fromDioException(exception);
+    }
+  }
+}
