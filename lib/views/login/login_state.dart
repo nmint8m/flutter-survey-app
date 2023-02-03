6517c064fac2b85c415a1fef89283df93a51ab1f
Tam Nguyen Minh <nmint8m@gmail.com>
2023-02-03 14:10:13 +0700

    [#38] Integrate backend for login

---
+++ lib/views/login/login_state.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'login_state.freezed.dart';
+
+@freezed
+class LoginState with _$LoginState {
+  const factory LoginState.init() = _Init;
+
+  const factory LoginState.loading() = _Loading;
+
+  const factory LoginState.error(String? error) = _Error;
+
+  const factory LoginState.success() = _Success;
+}
---
+++ lib/views/login/login_view_model.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kayla_flutter_ic/model/oauth_login.dart';
+import 'package:kayla_flutter_ic/usecases/base/base_use_case.dart';
+import 'package:kayla_flutter_ic/usecases/oath/login_use_case.dart';
+import 'package:kayla_flutter_ic/views/login/login_state.dart';
+
+class LoginViewModel extends StateNotifier<LoginState> {
+  final LoginUseCase _loginUseCase;
+
+  LoginViewModel(this._loginUseCase) : super(const LoginState.init());
+
+  void validateLoginData({
+    required String email,
+    required String password,
+  }) {}
+
+  void login({
+    required String email,
+    required String password,
+  }) async {
+    final result = await _loginUseCase.call(LoginParams(
+      email: email,
+      password: password,
+    ));
+    if (result is Success<OAuthLogin>) {
+      state = const LoginState.success();
+    } else {
+      _handleError(result as Failed);
+    }
+  }
+
+  void _handleError(Failed failure) {
+    state = LoginState.error(failure.errorMessage);
+  }
+}
