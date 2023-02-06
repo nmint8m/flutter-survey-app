a82a18589103a332421c346f9fe080c54dc7a74c
Tam Nguyen Minh <nmint8m@gmail.com>
2023-02-06 16:59:51 +0700

    [#8] Integrate storing tokens for later usage after calling OAuthLogin

--- lib/usecases/oath/login_use_case.dart
+++ lib/usecases/oath/login_use_case.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:injectable/injectable.dart';
 import 'package:kayla_flutter_ic/api/repository/oauth_repository.dart';
+import 'package:kayla_flutter_ic/api/storage/secure_storage.dart';
 import 'package:kayla_flutter_ic/model/oauth_login.dart';
 import 'package:kayla_flutter_ic/usecases/base/base_use_case.dart';
 
@@ -9,8 +10,12 @@ part 'login_params.dart';
 @Injectable()
 class LoginUseCase extends UseCase<OAuthLogin, LoginParams> {
   final OAuthRepository _repository;
+  final SecureStorage _secureStorage;
 
-  const LoginUseCase(this._repository);
+  const LoginUseCase(
+    this._repository,
+    this._secureStorage,
+  );
 
   @override
   Future<Result<OAuthLogin>> call(LoginParams params) async {
@@ -19,7 +24,37 @@ class LoginUseCase extends UseCase<OAuthLogin, LoginParams> {
         email: params.email,
         password: params.password,
       );
+      _storeTokens(result);
       return Success(result);
+    } catch (exception) {
+      return Failed(UseCaseException(exception));
+    }
+  }
+
+  Future<Result<void>> _storeTokens(OAuthLogin oauthLogin) async {
+    try {
+      await _secureStorage.storeId(oauthLogin.id);
+      await _secureStorage.storeTokenType(oauthLogin.tokenType);
+      await _secureStorage.storeAccessToken(oauthLogin.accessToken);
+      await _secureStorage.storeExpiresIn('${oauthLogin.expiresIn}');
+      await _secureStorage.storeRefreshToken(oauthLogin.refreshToken);
+
+      _secureStorage.id.then((value) {
+        print(value);
+      });
+      _secureStorage.tokenType.then((value) {
+        print(value);
+      });
+      _secureStorage.accessToken.then((value) {
+        print(value);
+      });
+      _secureStorage.expiresIn.then((value) {
+        print(value);
+      });
+      _secureStorage.refreshToken.then((value) {
+        print(value);
+      });
+      return Success(null);
     } catch (exception) {
       return Failed(UseCaseException(exception));
     }
