297af5053613a944ab038d979c587325868f7b23
Tam Nguyen Minh <nmint8m@gmail.com>
2023-02-13 12:57:50 +0700

    [#12] [#15] Add NavigationController

---
+++ lib/utils/navigation_controller.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:injectable/injectable.dart';
+import 'package:kayla_flutter_ic/di/di.dart';
+
+final navigationController = getIt.get<NavigationController>();
+
+@singleton
+class NavigationController with ChangeNotifier {
+  void push(BuildContext context, Widget widget) {
+    Route route =
+        MaterialPageRoute(builder: (context) => ProviderScope(child: widget));
+    Navigator.push(context, route);
+  }
+
+  void pop(BuildContext context) {
+    Navigator.pop(context);
+  }
+}
