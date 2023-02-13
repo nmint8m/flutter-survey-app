d66888bdecb2852cb9955881de85a5ef196ff9c2
Tam Nguyen Minh <nmint8m@gmail.com>
2023-02-13 16:40:14 +0700

    [#16] Add more durations

--- lib/utils/durations.dart
+++ lib/utils/durations.dart
@@ -1,3 +1,5 @@
 class Durations {
   static const Duration oneSecond = Duration(seconds: 1);
+  static const Duration threeSecond = Duration(seconds: 3);
+  static const Duration halfSecond = Duration(milliseconds: 500);
 }
