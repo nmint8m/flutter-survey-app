e8f47c6577500169c312859f357a814bfd38802b
Tam Nguyen Minh <nmint8m@gmail.com>
2023-02-15 13:47:34 +0700

    [#42] Add /me to the APIService

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:kayla_flutter_ic/api/response/me_response.dart';
+import 'package:kayla_flutter_ic/model/response/user_response.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'api_service.g.dart';
@@ -7,6 +8,9 @@ part 'api_service.g.dart';
 @RestApi()
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @GET('users')
+  Future<List<UserResponse>> getUsers();
 
   @GET('/api/v1/me')
   Future<MeResponse> me();
