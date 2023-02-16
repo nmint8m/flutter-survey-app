22bad37c0a79084b0e184f754c13aad47c660c55
Tam Nguyen Minh <nmint8m@gmail.com>
2023-02-16 14:30:43 +0700

    [#43] Create SurveyListResponse model and Survey model

--- lib/api/response/survey_response.dart
+++ lib/api/response/survey_response.dart
@@ -1,5 +1,6 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:kayla_flutter_ic/api/response/base_response_converter.dart';
+import 'package:kayla_flutter_ic/model/survey.dart';
 
 part 'survey_response.g.dart';
 
@@ -19,4 +20,11 @@ class SurveyResponse {
 
   factory SurveyResponse.fromJson(Map<String, dynamic> json) =>
       _$SurveyResponseFromJson(fromJsonApi(json));
+
+  Survey toSurvey() => Survey(
+        id: id,
+        title: title ?? '',
+        description: description ?? '',
+        coverImageUrl: coverImageUrl ?? '',
+      );
 }
