9846f76be8f2489f895ff56a51d83814d9c7f9c0
Tam Nguyen Minh <nmint8m@gmail.com>
2023-02-17 11:45:20 +0700

    [#18] [#20] Create SurveyPageIndicator

---
+++ lib/views/home/survey_section/survey_page_indicator.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class SurveyPageIndicator extends StatelessWidget {
+  final PageController controller;
+  final int count;
+
+  const SurveyPageIndicator({
+    super.key,
+    required this.controller,
+    required this.count,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: SmoothPageIndicator(
+        controller: controller,
+        count: count,
+        effect: const ColorTransitionEffect(
+          dotWidth: 8,
+          dotHeight: 8,
+          radius: 8,
+          spacing: 10,
+          dotColor: Colors.white30,
+          activeDotColor: Colors.white,
+        ),
+      ),
+    );
+  }
+}
