8feca9f1e4a6616ad77e3b5fbab36259974baefe
Tam Nguyen Minh <nmint8m@gmail.com>
2023-02-24 13:58:38 +0700

    [#23] UI for survey detail

--- lib/views/survey_detail/survey_detail_view.dart
+++ lib/views/survey_detail/survey_detail_view.dart
@@ -84,6 +84,70 @@ class SurveyDetailViewState extends ConsumerState<SurveyDetailView>
         },
       );
 
+  Widget get _background => SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Consumer(builder: (_, ref, __) {
+          // TODO: - Network image stream
+          return Image(image: Assets.images.nimbleBackground.image().image);
+        }),
+      );
+
+  Widget get _mainBody => Consumer(
+        builder: (_, ref, __) {
+          return Container(
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  // TODO: - Remove hard code
+                  "Bonchon Chicken",
+                  style: Theme.of(context).textTheme.displayMedium,
+                  maxLines: 2,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  // TODO: - Remove hard code
+                  "Do you wanna take a bite? Mlem.",
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  maxLines: 2,
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Spacer(),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(140, 56)),
+                      onPressed: () {},
+                      child: const Text('Start survey'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      );
+
+  Widget get _body => Consumer(
+        builder: (_, ref, __) {
+          // TODO: - Remove hard code for loading
+          return Stack(
+            children: [
+              _mainBody,
+              const SurveyDetailSkeletonLoading(),
+            ],
+          );
+        },
+      );
+
   @override
   Widget build(BuildContext context) {
     return Stack(
