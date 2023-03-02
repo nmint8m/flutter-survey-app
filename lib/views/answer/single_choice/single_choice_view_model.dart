d6ed4e4047c7d6c76703956908bb23ad215ad3d2
Tam Nguyen Minh <nmint8m@gmail.com>
2023-03-02 15:10:11 +0700

    [#27] Implement UI for the single answer of a multiple choice question

--- lib/views/answer/single_choice/single_choice_view_model.dart
+++ lib/views/answer/single_choice/single_choice_view_model.dart
@@ -1,22 +1,23 @@
-import 'dart:async';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kayla_flutter_ic/views/answer/single_choice/single_choice_option_ui_model.dart';
 import 'package:kayla_flutter_ic/views/answer/single_choice/single_choice_state.dart';
-import 'package:kayla_flutter_ic/views/answer/single_choice/single_choice_view.dart';
-
-final selectedIndexStream = StreamProvider.autoDispose<int>((ref) {
-  return ref
-      .watch(singleChoiceViewModelProvider.notifier)
-      ._selectedIndexStream
-      .stream;
-});
 
 class SingleChoiceViewModel extends StateNotifier<SingleChoiceState> {
-  final StreamController<int> _selectedIndexStream = StreamController();
-
+  List<SingleChoiceOptionUIModel> _uiModels = [];
   SingleChoiceViewModel() : super(const SingleChoiceState.init());
 
+  void setUpData(List<SingleChoiceOptionUIModel> uiModels) {
+    _uiModels = uiModels;
+    state = SingleChoiceState.select(
+      uiModels: _uiModels,
+      selectedIndex: null,
+    );
+  }
+
   void selectOption({required int index}) {
-    _selectedIndexStream.add(index);
+    state = SingleChoiceState.select(
+      uiModels: _uiModels,
+      selectedIndex: index,
+    );
   }
 }
