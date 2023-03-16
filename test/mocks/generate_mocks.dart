066ae1f037dc8804bb846b160eff3f14f81c034a
Tam Nguyen Minh <nmint8m@gmail.com>
2023-03-16 11:28:43 +0700

    [#38] Add tests for UserRepository

--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -2,18 +2,26 @@ import 'package:dio/dio.dart';
 import 'package:kayla_flutter_ic/api/api_service.dart';
 import 'package:kayla_flutter_ic/api/oauth_service.dart';
 import 'package:kayla_flutter_ic/api/repository/oauth_repository.dart';
+import 'package:kayla_flutter_ic/api/repository/user_repository.dart';
 import 'package:kayla_flutter_ic/api/storage/secure_storage.dart';
 import 'package:kayla_flutter_ic/usecases/base/base_use_case.dart';
 import 'package:kayla_flutter_ic/usecases/oath/login_use_case.dart';
+import 'package:kayla_flutter_ic/usecases/oath/logout_use_case.dart';
+import 'package:kayla_flutter_ic/usecases/survey/get_surveys_use_case.dart';
+import 'package:kayla_flutter_ic/usecases/user/get_profile_use_case.dart';
 import 'package:mockito/annotations.dart';
 
 @GenerateMocks([
   ApiService,
   OAuthService,
   OAuthRepository,
+  UserRepository,
   SecureStorage,
   UseCaseException,
   LoginUseCase,
+  LogoutUseCase,
+  GetProfileUseCase,
+  GetSurveysUseCase,
   DioError,
 ])
 main() {
