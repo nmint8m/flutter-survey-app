d9c7ce53ef7e5b31a755edd7bef3cad042c7c96a
Tam Nguyen Minh <nmint8m@gmail.com>
2023-03-16 18:13:16 +0700

    [#40] Add tests for HomeViewModel

--- test/view_model/home_view_model_test.dart
+++ test/view_model/home_view_model_test.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: deprecated_member_use
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kayla_flutter_ic/api/exception/network_exceptions.dart';
